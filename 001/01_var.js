7ac44ba17e6212b5028cc685d7b7b57d28b729e5
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-01-21 12:28:42 +0000

    varables

---
+++ 001/01_var.js
@@ -0,0 +1,21 @@
+const accountId=23421
+var address="kj college"
+let email="vedant@gmail.com"
+name="vedant"
+let accountName;
+
+//accountId=2 // not valid
+
+address="sangli"
+email="javascript@gmail.com"
+name="salunkhe"
+
+/*
+in javascript usually we dont use the var keyword
+case the problem of the scope instead we use the 
+'let' keyword
+*/
+
+console.log(accountId);
+
+console.table([accountId,address,email,name,accountName])
