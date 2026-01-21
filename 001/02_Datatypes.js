ed2fa2616b5190c704d8105b5908683b566b26f1
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-01-21 12:47:54 +0000

    Datatypes

---
+++ 001/02_Datatypes.js
@@ -0,0 +1,18 @@
+let name="vedant"
+let age=20
+let experience=null
+
+/* 
+mostly datatypes are similar to other programming languages 
+int
+bigint
+var
+string
+*/
+
+console.table([typeof "name",typeof age,typeof undefined, typeof experience])
+
+/*
+    the type of the 'null' is 'object'
+    the type of the 'undefinde' is 'undefined'
+*/
