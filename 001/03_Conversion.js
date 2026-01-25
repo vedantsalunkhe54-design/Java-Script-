e85b0afb3dd4bf9f67913d70aa9dfb1c7e2fe05a
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-01-25 13:02:30 +0000

    Conversion od Datatypes

---
+++ 001/03_Conversion.js
@@ -0,0 +1,30 @@
+let a="33 abc"
+
+//console.log(typeof a);
+//console.log(a);
+
+let b=Number(a)
+
+//console.log(typeof b);
+//console.log(b);
+
+//here the code is converted into number but shows output NaN
+
+let login=""
+
+let logout=Boolean(login)
+
+console.log(login);
+console.log(typeof logout);
+console.log(logout);
+
+// "" = false
+// 1 = true
+// "vedant" = true
+
+let x=33
+let z=String(x)
+
+console.log(x);
+console.log(typeof x);
+
