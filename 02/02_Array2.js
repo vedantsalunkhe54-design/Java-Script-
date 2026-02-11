f5d05e33cc3b74b0923016cc08924ab8c2fa479e
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-02-11 14:10:46 +0000

    Array2

---
+++ 02/02_Array2.js
@@ -0,0 +1,28 @@
+const marvel_heros=["Ironman","Spiderman","Captain America"];
+const dc_heros=["Superman","Batman","Wonder Woman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros[3][2]); // It will print "Wonder Woman"
+
+// let all_heros=marvel_heros.concat(dc_heros);
+// console.log(all_heros); 
+
+// let All_new_heros=[...marvel_heros,...dc_heros];
+// console.log(All_new_heros);
+
+// let new_realm_heros=[1,2,3,4,[5,6,7],8,9,[10,11,12,[13,14,15]]];
+// const flat_new_realm_heros=new_realm_heros.flat(Infinity);
+// console.log(flat_new_realm_heros);
+
+console.log(Array.isArray("Vedant"));
+
+console.log(Array.from("Salunkhe"));
+
+let score1= 100;
+let score2= 200;
+let score3= 300;
+let score4= 400;
+
+console.log(Array.of(score1,score2,score3,score4));
+
+
