05bd44a8bf7ad9ac9e713af83d7f18d487ff80b3
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-02-16 16:51:25 +0000

    Functions

---
+++ 03/02_Functions.js
@@ -0,0 +1,23 @@
+function calculateprice(...num){ // This is the rest used to add multiple values
+    return num;
+}
+
+console.log(calculateprice(34,45,21,45,634,3461));
+
+const user={
+    name:"Vedant",
+    age: 20,
+    interest: "JavaScript"
+}
+
+function uvalue(anyobject){
+    console.log(`The user name is ${anyobject.name} and his age is ${anyobject.age}`);
+}
+uvalue(user);
+
+const newarr=[200,400,600]
+
+function returnarr(mynewarr){
+    return mynewarr[1];
+}
+console.log(returnarr(newarr));
