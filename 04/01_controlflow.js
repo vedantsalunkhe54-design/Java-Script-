67ec2f0c698b004cb341991e4ade28688d5fb648
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-03-10 16:15:58 +0000

    control flow

---
+++ 04/01_controlflow.js
@@ -0,0 +1,46 @@
+if(true) console.log("prints the value"), console.log("THis will aslo print cause we put coma"),
+console.log("This will print even if its on next line cause of coma");
+
+if(""){
+    console.log("False statement");
+}
+
+else if(" "){
+    console.log("true statement");
+}
+
+else{
+    console.log("True or False");
+    
+}
+
+
+/*falsely values
+
+    0, -0, false, '', null, NaN, undefined , etc
+
+  Thruthy values
+    
+    1, Ture, ' ', "0", etc
+
+*/
+
+const day=3;
+
+
+
+switch(day){
+    case 1: console.log("Monday");
+    break;
+
+    case 2: console.log("Tuesday");
+    break;
+
+    case 3: console.log("Wednesday");
+    
+    break;
+
+    default: console.log("no value");
+    
+    
+}
