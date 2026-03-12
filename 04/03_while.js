47d2ac45280787d28f14b30892c4c304bebe5081
vedantsalunkhe54-design <vedantsalunkhe54@gmail.com>
2026-03-12 13:45:08 +0000

    While

---
+++ 04/03_while.js
@@ -0,0 +1,23 @@
+let index=0
+while (index<=10) {
+    console.log(index);
+    index=index+2
+}
+
+
+let arr=['mango','banana','apple','strawberry']
+
+let a=0
+while(a<arr.length){
+    console.log(`The value of array is ${arr[a]} & index is ${a}`);
+    a=a+1;
+}
+
+
+let score=1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+
+    
+} while (score<=10);
